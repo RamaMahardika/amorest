9b446f216ef9a7ecc111c7f67323adc415b072d5
Arifin Nurmardiansyah <arifinofficial30@gmail.com>
2018-09-14 13:19:03 +0800

    add route for gallery

--- startup/routes.js
+++ startup/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const index   = require('../routes/index.js');
 const content = require('../routes/content');
 const contentChild = require('../routes/content-child');
-const index   = require('../routes/index.js');
+const gallery = require('../routes/gallery');
+
 
 module.exports = function(app) {
   app.use('/', index);
   app.use(express.json());
   app.use('/api/v1/content', content);
   app.use('/api/v1/content-child', contentChild);
+  app.use('/api/v1/gallery', gallery);
 }
