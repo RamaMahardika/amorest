ff16f329222d55124d20e3f36ff153e228ac9f90
Arifin Nurmardiansyah <arifinofficial30@gmail.com>
2018-09-14 12:31:15 +0800

    add route for index

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express   = require('express');
+const router    = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Amo Spa API - Develop by Webfeat Team');
+});
+
+module.exports = router;
--- startup/routes.js
+++ startup/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const content = require('../routes/content');
 const contentChild = require('../routes/content-child');
+const index   = require('../routes/index.js');
 
 module.exports = function(app) {
+  app.use('/', index);
   app.use(express.json());
   app.use('/api/v1/content', content);
   app.use('/api/v1/content-child', contentChild);
