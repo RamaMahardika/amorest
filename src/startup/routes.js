d1b1b630852b05034038fd1c7aa9403462a75a7f
Arifin Nurmardiansyah <arifinofficial30@gmail.com>
2018-09-15 14:23:55 +0800

    update route and add default path

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -3,12 +3,17 @@ const index = require('../routes/index.js');
 const content = require('../routes/content');
 const contentChild = require('../routes/content-child');
 const gallery = require('../routes/gallery');
+const menuFood = require('../routes/menu-food');
+const reviews = require('../routes/reviews');
 
+const path = '/api/v1';
 
 module.exports = function(app) {
   app.use('/', index);
   app.use(express.json());
-  app.use('/api/v1/content', content);
-  app.use('/api/v1/content-child', contentChild);
-  app.use('/api/v1/gallery', gallery);
+  app.use(path + '/content', content);
+  app.use(path + '/content-child', contentChild);
+  app.use(path + '/gallery', gallery);
+  app.use(path + '/menu-food', menuFood);
+  app.use(path + '/reviews', reviews);
 }
